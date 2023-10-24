72319dc687d7ff8ca2f205ed285f3ed01bd10f35
somanie1 <chisomonyiwa@gmail.com>
2023-10-24 07:53:48 -0700

    AirBnB_clone_v2

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,16 +4,17 @@
 --   Grants all privileges for hbnb_test on hbnb_test_db.
 --   Grants SELECT privilege for hbnb_test on performance_schema.
 
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER
-    IF NOT EXISTS 'hbnb_test'@'localhost'
-    IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES
-   ON `hbnb_test_db`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Create the user if it doesn't exist and set password
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges on hbnb_test_db to hbnb_test
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to hbnb_test
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Flush privileges to apply the changes
 FLUSH PRIVILEGES;
